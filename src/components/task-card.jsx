839b8dd75247279c312ca22f166c5e143567b7d0
rihid <rizalhidayat006@gmail.com>
2024-07-16 16:07:31 +0700

    feat: add task status and modify task category

--- src/components/task-card.jsx
+++ src/components/task-card.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { ArrowRightIcon, TrashIcon } from '@heroicons/react/24/outline';
 
-function TaskCard({task, showEditForm, deleteTask}) {
+function TaskCard({ task, showEditForm, deleteTask }) {
   return (
     <div className="relative flex flex-col justify-beetwen gap-2 rounded-xl bg-background px-8 py-6 border border-foreground/30 hover:border-foreground/60 hover:shadow-lg"
     >
       <h5 className="text-xl leading-7 font-semibold text-foreground">{task.name}</h5>
+      <div className="flex items-center">
+        <span className="inline-block capitalize text-sm rounded-full px-2 py-1 text-center bg-slate-100 text-foreground">{task.category}</span>
+      </div>
       <div className=" text-slate-500 my-2">
         <p>{task.description}</p>
       </div>
       <div className="flex justify-between items-center gap-2 my-2">
-        <span className="inline-block bg-green-100 text-foreground text-sm rounded-full px-2 py-1 text-center">
-          {task.category}
+        <span className={`
+          inline-block capitalize text-sm rounded-full px-2 py-1 text-center
+          ${task.status === 'todo' ? 'bg-amber-500 text-background' : ''}
+          ${task.status === 'doing' ? 'bg-green-500 text-background' : ''}
+          ${task.status === 'done' ? 'bg-blue-500 text-background' : ''}
+        `}>
+          {task.status}
         </span>
         <button
           type='button'
@@ -22,7 +30,7 @@ function TaskCard({task, showEditForm, deleteTask}) {
         </button>
       </div>
       <div className="absolute right-1 top-1 flex">
-        <button 
+        <button
           type='button'
           onClick={() => showEditForm(task)}
           className="flex items-center justify-center bg-slate-100 hover:bg-slate-200 w-12 h-12 p-2 rounded-full cursor-pointer"
