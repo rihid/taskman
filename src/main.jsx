3447770b5a47a84a29b9b5268854be0c39d23687
rihid <rizalhidayat006@gmail.com>
2024-07-18 14:04:31 +0700

    configure routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import HomePage from "./pages/home-page";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>login</div>
+  },
+  {
+    path: "/board",
+    element: <HomePage />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
