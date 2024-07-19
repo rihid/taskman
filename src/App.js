3a1278aa969e2ebe191a197e747c112e700d4757
rihid <rizalhidayat006@gmail.com>
2024-07-20 00:10:25 +0700

    feat: add auth provider for login feature

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AuthProvider from './auth/auth-provider';
+
+function App() {
+  return (
+    <div className="App">
+      <AuthProvider></AuthProvider>
+    </div>
+  )
+}
+
+export default App;
