09327c9179d904caca69e888bd748b720cae2495
rihid <rizalhidayat006@gmail.com>
2024-07-19 21:39:23 +0700

    feat: modify border radius

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -7,7 +7,7 @@ function Button({className, type, color, size, onClick, disabled, label, childre
       className={`${className} w-full rounded-full whitespace-nowrap
       ${size === 'icon' ? 'p-1' : 'px-6 py-2 '}
 
-      ${color === 'primary' ? 'bg-green-500 hover:bg-green-600 border rounded-full text-background' : ''}
+      ${color === 'primary' ? 'bg-green-500 hover:bg-green-600 border rounded-xl text-background' : ''}
       ${color === 'secondary' ? 'bg-slate-200 hover:bg-slate-300 text-foreground' : ''}
       ${color === 'danger' ? 'bg-red-500 hover:bg-red-600 border-red-500 rounded-full text-white': ''}
     `}
